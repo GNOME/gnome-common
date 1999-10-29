8c686af4f60f006d9e25df5f3beb379ca8045a92
Elliot Lee <sopwith@src.gnome.org>
1999-10-29 19:16:05 +0000

    builds now. Integrate new popt. Don't install this library. Link
    * liboafgnome/: builds now.
    * support/: Integrate new popt. Don't install this library.
    * libgnome/Makefile.am: Link ../support/libgnomesupport.la in as part of libgnome.la
    * */Makefile.am: Reflect this
    * configure.in: Use AM_PATH_ORBIT macro
    
    svn path=/trunk/; revision=984

--- support/poptint.h
+++ support/poptint.h
@@ -5,13 +5,30 @@
 #ifndef H_POPTINT
 #define H_POPTINT
 
+/* Bit mask macros. */
+typedef	unsigned int __pbm_bits;
+#define	__PBM_NBITS		(8 * sizeof (__pbm_bits))
+#define	__PBM_IX(d)		((d) / __PBM_NBITS)
+#define __PBM_MASK(d)		((__pbm_bits) 1 << ((d) % __PBM_NBITS))
+typedef struct {
+    __pbm_bits bits[1];
+} pbm_set;
+#define	__PBM_BITS(set)	((set)->bits)
+
+#define	PBM_ALLOC(d)	calloc(__PBM_IX (d) + 1, sizeof(__pbm_bits))
+#define	PBM_FREE(s)	free(s);
+#define PBM_SET(d, s)   (__PBM_BITS (s)[__PBM_IX (d)] |= __PBM_MASK (d))
+#define PBM_CLR(d, s)   (__PBM_BITS (s)[__PBM_IX (d)] &= ~__PBM_MASK (d))
+#define PBM_ISSET(d, s) ((__PBM_BITS (s)[__PBM_IX (d)] & __PBM_MASK (d)) != 0)
+
 struct optionStackEntry {
     int argc;
-    const char ** argv;
+    /*@only@*/ const char ** argv;
+    /*@only@*/ pbm_set * argb;
     int next;
-    const char * nextArg;
-    const char * nextCharArg;
-    struct poptAlias * currAlias;
+    /*@only@*/ const char * nextArg;
+    /*@keep@*/ const char * nextCharArg;
+    /*@dependent@*/ struct poptAlias * currAlias;
     int stuffed;
 };
 
@@ -22,25 +39,26 @@ struct execEntry {
 };
 
 struct poptContext_s {
-    struct optionStackEntry optionStack[POPT_OPTION_DEPTH], * os;
-    const char ** leftovers;
+    struct optionStackEntry optionStack[POPT_OPTION_DEPTH];
+    /*@dependent@*/ struct optionStackEntry * os;
+    /*@owned@*/ const char ** leftovers;
     int numLeftovers;
     int nextLeftover;
-    const struct poptOption * options;
+    /*@keep@*/ const struct poptOption * options;
     int restLeftover;
-    const char * appName;
-    struct poptAlias * aliases;
+    /*@only@*/ const char * appName;
+    /*@only@*/ struct poptAlias * aliases;
     int numAliases;
     int flags;
     struct execEntry * execs;
     int numExecs;
-    const char ** finalArgv;
+    /*@only@*/ const char ** finalArgv;
     int finalArgvCount;
     int finalArgvAlloced;
-    struct execEntry * doExec;
-    const char * execPath;
+    /*@dependent@*/ struct execEntry * doExec;
+    /*@only@*/ const char * execPath;
     int execAbsolute;
-    const char * otherHelp;
+    /*@only@*/ const char * otherHelp;
 };
 
 #define	xfree(_a)	free((void *)_a)
@@ -49,13 +67,13 @@ struct poptContext_s {
 #include <libintl.h>
 #endif
 
-#ifdef HAVE_GETTEXT
+#if defined(HAVE_GETTEXT) && !defined(__LCLINT__)
 #define _(foo) gettext(foo)
 #else
 #define _(foo) (foo)
 #endif
 
-#ifdef HAVE_DGETTEXT
+#if defined(HAVE_DGETTEXT) && !defined(__LCLINT__)
 #define D_(dom, str) dgettext(dom, str)
 #define POPT_(foo) D_("popt", foo)
 #else
