80a465e548278e727aee556eab719e70979435b0
Martin Baulig <martin@src.gnome.org>
2000-07-24 15:55:43 +0000

    Don't use gnome-config if GNOME_DATADIR is set.
    svn path=/trunk/; revision=1317

--- bin/gnome-autogen.sh
+++ bin/gnome-autogen.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
+if test -z "$GNOME_DATADIR" ; then
+  GNOME_DATADIR=`gnome-config --datadir`
+fi
+
 if test -n "$USE_GNOME2_MACROS" ; then
-  export GNOME_COMMON_MACROS_DIR=`gnome-config --datadir`/aclocal/gnome2-macros
+  export GNOME_COMMON_MACROS_DIR="$GNOME_DATADIR/aclocal/gnome2-macros"
 else
-  export GNOME_COMMON_MACROS_DIR=`gnome-config --datadir`/aclocal/gnome-macros
+  export GNOME_COMMON_MACROS_DIR="$GNOME_DATADIR/aclocal/gnome-macros"
 fi
 
 export ACLOCAL_FLAGS="-I $GNOME_COMMON_MACROS_DIR $ACLOCAL_FLAGS"
