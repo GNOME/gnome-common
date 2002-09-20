a59c10eadf186e09be1efb90c0988df96070285c
Naba Kumar <naba@src.gnome.org>
2002-09-20 13:04:52 +0000

    Reviewed by: Naba Kumar <kh_naba@gnome.org>
    	* README: Updated README
    	* plugins/class-gen/clsGen.c, src/message_manager.cc
    	  , src/mainmeu_callbacks.c, src/memory.c
    	  , src/project_dbase_gui.c, src/text_editor_gui.c
    	  , widgets/gnomefilelist.c: Set window icons
    	* configure.in, macros/*, */Makefile.am, anjuta.spec.in:
    	  Enabled release build (stripped + optimized binaries, etc.)
    	* data/properties/anjuta.properties, src/commands.c,
    	  src/defaults.cFixed bug #595489 (stdin on executables)
    	* src/appwizard.c, src/main.c, src/message-manager.cc
    	  , src/project_dbase.c, src/toolbar.c, widgets/gnomefilelist.c
    	  , src/debug_tree.c: Memory related (valgrind) fixes.
    	* src/message-manager-private.[cc,h], src/pixpams.h
    	  , src/preferences.[ch], src/preferences_gui.c: Terminal preferences
    	* anjuta.prj: Added debug_tree.c to SOURCES
    	* src/main_menubar_def.h, src/mainmenu_callbacks.[ch],
    	  src/text_editor_menu.c, src/text_editor_gui.c, src/anjuta.c:
    	  Compacted code and cleaned up unnecessary functions.
    	* tagmanager/*.[ch]: Code fixes.
    
    svn path=/branches/ANJUTA_1_0_0/; revision=2589

--- macros/autogen.sh
+++ macros/autogen.sh
@@ -96,7 +96,7 @@ fi
 if test -z "$*"; then
   echo "**Warning**: I am going to run \`configure' with no arguments."
   echo "If you wish to pass any to it, please specify them on the"
-  echo \`$0\'" command line."
+  echo "\`$0\' command line."
   echo
 fi
 
@@ -196,7 +196,7 @@ do
   fi
 done
 
-conf_flags="--enable-maintainer-mode --enable-compile-warnings" #--enable-iso-c
+conf_flags="--enable-maintainer-mode --enable-debug --enable-warnings" #--enable-iso-c
 
 if test x$NOCONFIGURE = x; then
   echo Running $srcdir/configure $conf_flags "$@" ...
