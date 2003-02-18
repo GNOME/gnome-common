e4d093be993de45b46aef5201924ddda643e859a
James Henstridge <james@daa.com.au>
2003-02-18 15:09:45 +0000

    check to see if gnome-deprecated-macros.m4 ended up in aclocal.m4, and
    2003-02-18  James Henstridge  <james@daa.com.au>
    
        * autogen.sh (topdir): check to see if gnome-deprecated-macros.m4
        ended up in aclocal.m4, and warn if so.
    
        * gnome-platform.m4: kill this too (with a deprecation warning).
    
        * gnome-cxx-check.m4: move to gnome-deprecated-macros.m4
    
        * gnome-pkgconfig.m4: remove, and add a deprecation warning in its
        place.
    
        * gnome-x-checks.m4: remove, and add an AU_DEFUN in its place
        telling people not to use it.
    
        * linger.m4: removed outright.
    
        * gnome-common.m4: remove GNOME_GTKDOC_CHECK, deprecated in favour
        of GTK_DOC_CHECK.
    
        * gnome-gettext.m4: remove file (people should be using gettextize
        or glib-gettextize).
    
        * gnome-deprecated-macros.m4: new file to hold deprecated macros.
    
    svn path=/trunk/; revision=2768

--- macros2/autogen.sh
+++ macros2/autogen.sh
@@ -1,216 +1,317 @@
 #!/bin/sh
 # Run this to generate all the initial makefiles, etc.
 
+#name of package
+PACKAGE=${PACKAGE:-Package}
+srcdir=${srcdir:-.}
+
+# default version requirements ...
+REQUIRED_AUTOCONF_VERSION=${REQUIRED_AUTOCONF_VERSION:-2.53}
+REQUIRED_AUTOMAKE_VERSION=${REQUIRED_AUTOMAKE_VERSION:-1.4}
+REQUIRED_LIBTOOL_VERSION=${REQUIRED_LIBTOOL_VERSION:-1.4.3}
+REQUIRED_GETTEXT_VERSION=${REQUIRED_GETTEXT_VERSION:-0.10.40}
+REQUIRED_GLIB_GETTEXT_VERSION=${REQUIRED_GLIB_GETTEXT_VERSION:-2.2.0}
+REQUIRED_INTLTOOL_VERSION=${REQUIRED_INTLTOOL_VERSION:-0.25}
+REQUIRED_PKG_CONFIG_VERSION=${REQUIRED_PKG_CONFIG_VERSION:-0.14.0}
+REQUIRED_GTK_DOC_VERSION=${REQUIRED_GTK_DOC_VERSION:-1.0}
+
+# a list of required m4 macros.  Package can set an initial value
+REQUIRED_M4MACROS=${REQUIRED_M4MACROS:-}
+
+# if GNOME2_DIR set, modify ACLOCAL_FLAGS ...
+if [ -n "$GNOME2_DIR" ]; then
+    ACLOCAL_FLAGS="-I $GNOME2_DIR/share/aclocal $ACLOCAL_FLAGS"
+    LD_LIBRARY_PATH="$GNOME2_DIR/lib:$LD_LIBRARY_PATH"
+    PATH="$GNOME2_DIR/bin:$PATH"
+    export PATH
+    export LD_LIBRARY_PATH
+fi
+
+
+# some terminal codes ...
+boldface="`tput bold 2>/dev/null`"
+normal="`tput sgr0 2>/dev/null`"
+printbold() {
+    echo -n "$boldface"
+    echo "$@"
+    echo -n "$normal"
+}    
+printerr() {
+    echo "$@" >&2
+}
+
+# Usage:
+#     compare_versions MIN_VERSION ACTUAL_VERSION
+# returns true if ACTUAL_VERSION >= MIN_VERSION
+compare_versions() {
+    local min_version actual_version status save_IFS cur min
+    min_version=$1
+    actual_version=$2
+    status=0
+    IFS="${IFS=         }"; save_IFS="$IFS"; IFS="."
+    set $actual_version
+    for min in $min_version; do
+        cur=`echo $1 | sed 's/[^0-9].*$//'`; shift # remove letter suffixes
+        if [ -z "$min" ]; then break; fi
+        if [ -z "$cur" ]; then status=1; break; fi
+        if [ $cur -gt $min ]; then break; fi
+        if [ $cur -lt $min ]; then status=1; break; fi
+    done
+    IFS="$save_IFS"
+    return $status
+}
+
+# Usage:
+#     version_check PACKAGE VARIABLE CHECKPROGS MIN_VERSION SOURCE
+# checks to see if the package is available
+version_check() {
+    local package variable checkprogs min_version source status checkprog actual_version
+    package=$1
+    variable=$2
+    checkprogs=$3
+    min_version=$4
+    source=$5
+    status=1
+
+    printbold "checking for $package >= $min_version..."
+    for checkprog in $checkprogs; do
+	echo -n "  testing $checkprog... "
+	if $checkprog --version < /dev/null > /dev/null 2>&1; then
+	    actual_version=`$checkprog --version | head -1 | \
+                               sed 's/^.*[ 	]\([0-9.]*[a-z]*\).*$/\1/'`
+	    if compare_versions $min_version $actual_version; then
+		echo "found."
+		# set variable
+		eval "$variable=$checkprog"
+		status=0
+		break
+	    else
+		echo "too old (found version $actual_version)"
+	    fi
+	else
+	    echo "not found."
+	fi
+    done
+    if [ "$status" != 0 ]; then
+	printerr "***Error***: You must have $package >= $min_version installed"
+	printerr "  to build $PKG_NAME.  Download the appropriate package for"
+	printerr "  from your distribution or get the source tarball at"
+        printerr "    $source"
+	printerr
+    fi
+    return $status
+}
+
+# Usage:
+#     require_m4macro filename.m4
+# adds filename.m4 to the list of required macros
+require_m4macro() {
+    case "$REQUIRED_M4MACROS" in
+	$1\ * | *\ $1\ * | *\ $1) ;;
+	*) REQUIRED_M4MACROS="$REQUIRED_M4MACROS $1" ;;
+    esac
+}
+
+# Usage:
+#     check_m4macros
+# Checks that all the requested macro files are in the aclocal macro path
+# Uses REQUIRED_M4MACROS and ACLOCAL variables.
+check_m4macros() {
+    local macrodirs status macro dir macrofound
+
+    if [ -z "$REQUIRED_M4MACROS" ]; then
+	return 0
+    fi
+    # construct list of macro directories
+    macrodirs="`$ACLOCAL --print-ac-dir`"
+    set - $ACLOCAL_FLAGS
+    while [ $# -gt 0 ]; do
+	if [ "$1" = "-I" ]; then
+	    macrodirs="$macrodirs $2"
+	    shift
+	fi
+	shift
+    done
+
+    status=0
+    printbold "Checking for required M4 macros..."
+    # check that each macro file is in one of the macro dirs
+    for macro in $REQUIRED_M4MACROS; do
+	macrofound=false
+	for dir in $macrodirs; do
+	    if [ -f "$dir/$macro" ]; then
+		macrofound=true
+		break
+	    fi
+	done
+	if $macrofound; then
+	    :
+	else
+	    printerr "  $macro not found"
+	    status=1
+	fi
+    done
+    if [ "$status" != 0 ]; then
+	printerr "***Error***: some autoconf macros required to build $PKG_NAME"
+	printerr "  were not found in your aclocal path.  Perhaps you need to"
+	printerr "  adjust your ACLOCAL_PATH?"
+        printerr "    $source"
+	printerr
+    fi
+    return $status
+}
+
+want_libtool=false
+want_gettext=false
+want_glib_gettext=false
+want_intltool=false
+want_pkg_config=false
+want_gtk_doc=false
+
+configure_files="`find $srcdir -name configure.ac -print -or -name configure.in -print`"
+for configure_ac in $configure_files; do
+    if grep "^A[CM]_PROG_LIBTOOL" $configure_ac >/dev/null; then
+	want_libtool=true
+    fi
+    if grep "^AM_GNU_GETTEXT" $configure_ac >/dev/null; then
+	want_gettext=true
+    fi
+    if grep "^AM_GLIB_GNU_GETTEXT" $configure_ac >/dev/null; then
+	want_glib_gettext=true
+    fi
+    if grep "^AC_PROG_INTLTOOL" $configure_ac >/dev/null; then
+	want_intltool=true
+    fi
+    if grep "^PKG_CHECK_MODULES" $configure_ac >/dev/null; then
+	want_pkg_config=true
+    fi
+    if grep "^GTK_DOC_CHECK" $configure_ac >/dev/null; then
+	want_gtk_doc=true
+    fi
+done
+
 DIE=0
 
-if [ -n "$GNOME2_DIR" ]; then
-	ACLOCAL_FLAGS="-I $GNOME2_DIR/share/aclocal $ACLOCAL_FLAGS"
-	LD_LIBRARY_PATH="$GNOME2_DIR/lib:$LD_LIBRARY_PATH"
-	PATH="$GNOME2_DIR/bin:$PATH"
-	export PATH
-	export LD_LIBRARY_PATH
+version_check autoconf AUTOCONF 'autoconf autoconf-2.53' $REQUIRED_AUTOCONF_VERSION \
+    "http://ftp.gnu.org/pub/gnu/autoconf/autoconf-$REQUIRED_AUTOCONF_VERSION.tar.gz" || DIE=1
+AUTOHEADER=`echo $AUTOCONF | sed s/autoconf/autoheader/`
+
+case $REQUIRED_AUTOMAKE_VERSION in
+    1.4*) automake_progs="automake-1.4" ;;
+    1.5*) automake_progs="automake-1.7 automake-1.6 automake-1.5" ;;
+    1.6*) automake_progs="automake-1.7 automake-1.6" ;;
+    1.7*) automake_progs="automake-1.7" ;;
+esac
+version_check automake AUTOMAKE "$automake_progs" $REQUIRED_AUTOMAKE_VERSION \
+    "http://ftp.gnu.org/pub/gnu/automake/automake-$REQUIRED_AUTOMAKE_VERSION.tar.gz" || DIE=1
+ACLOCAL=`echo $AUTOMAKE | sed s/automake/aclocal/`
+
+if $want_libtool; then
+    version_check libtool LIBTOOLIZE libtoolize $REQUIRED_LIBTOOL_VERSION \
+        "http://ftp.gnu.org/pub/gnu/libtool/libtool-$REQUIRED_LIBTOOL_VERSION.tar.gz" || DIE=1
+    require_m4macro libtool.m4
 fi
 
-(autoconf --version) < /dev/null > /dev/null 2>&1 || {
-  echo
-  echo "**Error**: You must have \`autoconf' installed to compile $PKG_NAME."
-  echo "Download the appropriate package for your distribution,"
-  echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/"
-  DIE=1
-}
-
-# Determine whether to use configure.in or configure.ac
-
-if [ -f $srcdir/configure.ac ]; then
-  CONFIGURE="$srcdir/configure.ac"
-else
-  CONFIGURE="$srcdir/configure.in"
+if $want_gettext; then
+    version_check gettext GETTEXTIZE gettextize $REQUIRED_GETTEXT_VERSION \
+        "http://ftp.gnu.org/pub/gnu/gettext/gettext-$REQUIRED_GETTEXT_VERSION.tar.gz" || DIE=1
+    require_m4macro gettext.m4
 fi
 
-(grep "^AC_PROG_INTLTOOL" "$CONFIGURE" >/dev/null) && {
-  (intltoolize --version) < /dev/null > /dev/null 2>&1 || {
-    echo
-    echo "**Error**: You must have \`intltoolize' installed to compile $PKG_NAME."
-    echo "Get ftp://ftp.gnome.org/pub/GNOME/stable/sources/intltool/intltool-0.10.tar.gz"
-    echo "(or a newer version if it is available)"
-    DIE=1
-  }
-}
-
-(grep "^AM_PROG_XML_I18N_TOOLS" "$CONFIGURE" >/dev/null) && {
-  (xml-i18n-toolize --version) < /dev/null > /dev/null 2>&1 || {
-    echo
-    echo "**Error**: You must have \`xml-i18n-toolize' installed to compile $PKG_NAME."
-    echo "Get ftp://ftp.gnome.org/pub/GNOME/stable/sources/xml-i18n-tools/xml-i18n-tools-0.9.tar.gz"
-    echo "(or a newer version of xml-i18n-tools or intltool if it is available)"
-    DIE=1
-  }
-}
-
-(grep "^AM_PROG_LIBTOOL" "$CONFIGURE" >/dev/null) && {
-  (libtool --version) < /dev/null > /dev/null 2>&1 || {
-    echo
-    echo "**Error**: You must have \`libtool' installed to compile $PKG_NAME."
-    echo "Get ftp://ftp.gnu.org/pub/gnu/libtool-1.2d.tar.gz"
-    echo "(or a newer version if it is available)"
-    DIE=1
-  }
-}
-
-
-if grep "^AM_[A-Z0-9_]\{1,\}_GETTEXT" "$CONFIGURE" >/dev/null; then
-  if grep "sed.*POTFILES" "$CONFIGURE" >/dev/null; then
-    GETTEXTIZE=""
-  else
-    if grep "^AM_GLIB_GNU_GETTEXT" "$CONFIGURE" >/dev/null; then
-      GETTEXTIZE="glib-gettextize"
-      GETTEXTIZE_URL="ftp://ftp.gtk.org/pub/gtk/v1.3/glib-1.3.11.tar.gz"
-    else
-      GETTEXTIZE="gettextize"
-      GETTEXTIZE_URL="ftp://alpha.gnu.org/gnu/gettext-0.10.35.tar.gz"
-    fi
-
-    $GETTEXTIZE --version < /dev/null > /dev/null 2>&1
-    if test $? -ne 0; then
-      echo
-      echo "**Error**: You must have \`$GETTEXTIZE' installed to compile $PKG_NAME."
-      echo "Get $GETTEXTIZE_URL"
-      echo "(or a newer version if it is available)"
-      DIE=1
-    fi
-  fi
+if $want_glib_gettextize; then
+    version_check glib-gettext GLIB_GETTEXTIZE glib-gettextize $REQUIRED_GLIB_GETTEXT_VERSION \
+        "ftp://ftp.gtk.org/pub/gtk/v2.2/glib-$REQUIRED_GLIB_GETTEXT_VERSION.tar.gz" || DIE=1
+    require_m4macro glib-gettext.m4
 fi
 
-(automake-1.4 --version) < /dev/null > /dev/null 2>&1 || {
-  echo
-  echo "**Error**: You must have \`automake' installed to compile $PKG_NAME."
-  echo "Get ftp://ftp.gnu.org/pub/gnu/automake-1.3.tar.gz"
-  echo "(or a newer version if it is available)"
-  DIE=1
-  NO_AUTOMAKE=yes
-}
+if $want_intltool; then
+    version_check intltool INTLTOOLIZE intltoolize $REQUIRED_INTLTOOL_VERSION \
+        "http://ftp.gnome.org/pub/GNOME/sources/intltool/" || DIE=1
+    require_m4macro intltool.m4
+fi
 
+if $want_pkg_config; then
+    version_check pkg-config PKG_CONFIG pkg-config $REQUIRED_PKG_CONFIG_VERSION \
+        "'http://www.freedesktop.org/software/pkgconfig/releases/pkgconfig-$REQUIRED_PKG_CONFIG_VERSION.tar.gz" || DIE=1
+    require_m4macro pkg.m4
+fi
 
-# if no automake, don't bother testing for aclocal
-test -n "$NO_AUTOMAKE" || (aclocal-1.4 --version) < /dev/null > /dev/null 2>&1 || {
-  echo
-  echo "**Error**: Missing \`aclocal'.  The version of \`automake'"
-  echo "installed doesn't appear recent enough."
-  echo "Get ftp://ftp.gnu.org/pub/gnu/automake-1.3.tar.gz"
-  echo "(or a newer version if it is available)"
-  DIE=1
-}
+if $want_gtk_doc; then
+    version_check gtk-doc GTKDOCIZE gtkdocize $REQUIRED_GTK_DOC_VERSION \
+        "http://ftp.gnome.org/pub/GNOME/sources/gtk-doc/" || DIE=1
+    require_m4macro gtk-doc.m4
+fi
 
-if test "$DIE" -eq 1; then
+check_m4macros || DIE=1
+
+if [ "$DIE" -eq 1 ]; then
   exit 1
 fi
 
 if test -z "$*"; then
-  echo "**Warning**: I am going to run \`configure' with no arguments."
-  echo "If you wish to pass any to it, please specify them on the"
-  echo \`$0\'" command line."
-  echo
+  printerr "**Warning**: I am going to run \`configure' with no arguments."
+  printerr "If you wish to pass any to it, please specify them on the"
+  printerr \`$0\'" command line."
+  printerr
 fi
 
-case $CC in
-xlc )
-  am_opt=--include-deps;;
-esac
+topdir=`pwd`
+for configure_ac in $configure_files; do 
+    dirname=`dirname $configure_ac`
+    basename=`basename $configure_ac`
+    if test -f $dirname/NO-AUTO-GEN; then
+	echo skipping $dirname -- flagged as no auto-gen
+    else
+	printbold "Processing $dirname"
 
-for coin in `find $srcdir -name configure.in -print -o -name configure.ac -print`
-do 
-  dr=`dirname $coin`
-  bn=`basename $coin`
-  if test -f $dr/NO-AUTO-GEN; then
-    echo skipping $dr -- flagged as no auto-gen
-  else
-    echo processing $dr
-    ( cd $dr
+	aclocalinclude="$ACLOCAL_FLAGS"
+	printbold "Running $ACLOCAL..."
+	$ACLOCAL $aclocalinclude || exit 1
 
-      aclocalinclude="$ACLOCAL_FLAGS"
-
-      if test "$GETTEXTIZE"; then
-	echo "Creating $dr/aclocal.m4 ..."
-	test -r $dr/aclocal.m4 || touch $dr/aclocal.m4
-	echo "Running $GETTEXTIZE...  Ignore non-fatal messages."
-	echo "no" | $GETTEXTIZE --force --copy
-	echo "Making $dr/aclocal.m4 writable ..."
-	test -r $dr/aclocal.m4 && chmod u+w $dr/aclocal.m4
-      fi
-      if grep "^AC_PROG_INTLTOOL" $bn >/dev/null; then
-        echo "Running intltoolize..."
-	intltoolize --copy --force --automake
-      fi
-      if grep "^AM_PROG_XML_I18N_TOOLS" $bn >/dev/null; then
-        echo "Running xml-i18n-toolize..."
-	xml-i18n-toolize --copy --force --automake
-      fi
-      if grep "^AM_PROG_LIBTOOL" $bn >/dev/null; then
-	if test -z "$NO_LIBTOOLIZE" ; then 
-	  echo "Running libtoolize..."
-	  libtoolize --force --copy
+	if grep "GNOME_AUTOGEN_OBSOLETE" aclocal.m4 >/dev/null; then
+	    printerr "*** obsolete gnome macros were used in $configure_ac"
 	fi
-      fi
 
-      echo "Running aclocal $aclocalinclude ..."
-      aclocal-1.4 $aclocalinclude || {
-        echo
-	echo "**Error**: \`aclocal' failed. Please fix the warnings"
-        echo "(probably missing development files) and try again."
-	DIE=1
-      }
+	if grep "^A[CM]_PROG_LIBTOOL" $basename >/dev/null; then
+	    printbold "Running $LIBTOOLIZE..."
+	    $LIBTOOLIZE --force || exit 1
+	fi
+	if grep "^AM_GLIB_GNU_GETTEXT" $basename >/dev/null; then
+	    printbold "Running $GLIB_GETTEXTIZE... Ignore non-fatal messages."
+	    echo "no" | $GLIB_GETTEXTIZE --force --copy || exit 1
+	elif grep "^AM_GNU_GETTEXT" $basename >/dev/null; then
+	    printbold "Running $GETTEXTIZE... Ignore non-fatal messages."
+	    echo "no" | $GETTEXTIZE --force --copy || exit 1
+	fi
+	if grep "^AC_PROG_INTLTOOL" $basename >/dev/null; then
+	    printbold "Running $INTLTOOLIZE..."
+	    $INTLTOOLIZE --force --automake || exit 1
+	fi
+	if grep "^GTK_DOC_CHECK" $basename >/dev/null; then
+	    printbold "Running $GTKDOCIZE..."
+	    $GTKDOCIZE || exit 1
+	fi
+	if grep "^A[CM]_CONFIG_HEADER" $basename >/dev/null; then
+	    printbold "Running $AUTOHEADER..."
+	    $AUTOHEADER || exit 1
+	fi
 
-      if test "$DIE" -eq 1; then
-        exit 1
-      fi
+	printbold "Running $AUTOMAKE..."
+	$AUTOMAKE --gnu --add-missing || exit 1
 
-      if grep "^AM_CONFIG_HEADER" $bn >/dev/null; then
-	echo "Running autoheader..."
-	autoheader || {
-          echo
-          echo "**Error**: \`autoheader' failed. Please fix the warnings"
-	  echo "(probably missing development files) and try again."
-	  DIE=1
-      }
+	printbold "Running $AUTOCONF..."
+	$AUTOCONF || exit 1
 
-      if test "$DIE" -eq 1; then
-        exit 1
-      fi
-
-      fi
-      echo "Running automake --gnu $am_opt ..."
-      automake-1.4 --add-missing --gnu $am_opt || {
-        echo
-	echo "**Error**: \`automake' failed. Please fix the warnings"
-	echo "(probably missing development files) and try again."
-	DIE=1
-      }
-
-      if test "$DIE" -eq 1; then
-        exit 1
-      fi
-
-      echo "Running autoconf ..."
-      autoconf || {
-        echo
-	echo "**Error**: \`autoconf' failed. Please fix the warnings"
-	echo "(probably missing development files) and try again."
-	DIE=1
-      }
-
-      if test "$DIE" -eq 1; then
-        exit 1
-      fi
-    )
-  fi
+	cd $topdir
+    fi
 done
 
-conf_flags="--enable-maintainer-mode --enable-compile-warnings" #--enable-iso-c
+conf_flags="--enable-maintainer-mode --enable-compile-warnings"
 
 if test x$NOCONFIGURE = x; then
-  echo Running $srcdir/configure $conf_flags "$@" ...
-  $srcdir/configure $conf_flags "$@" \
-  && echo Now type \`make\' to compile $PKG_NAME || exit 1
+    printbold Running $srcdir/configure $conf_flags "$@" ...
+    $srcdir/configure $conf_flags "$@" \
+	&& echo Now type \`make\' to compile $PKG_NAME || exit 1
 else
-  echo Skipping configure process.
+    echo Skipping configure process.
 fi
--- macros2/gnome-common.m4
+++ macros2/gnome-common.m4
@@ -19,28 +19,6 @@ AC_DEFUN([GNOME_COMMON_INIT],
 	AM_CONDITIONAL(INSIDE_GNOME_DOCU, false)
 ])
 
-AC_DEFUN([GNOME_GTKDOC_CHECK],
-[
-	AC_CHECK_PROG(GTKDOC, gtkdoc-mkdb, true, false)
-	AM_CONDITIONAL(HAVE_GTK_DOC, $GTKDOC)
-	AC_SUBST(HAVE_GTK_DOC)
-
-	dnl Let people disable the gtk-doc stuff.
-	AC_ARG_ENABLE(gtk-doc, [  --enable-gtk-doc  Use gtk-doc to build documentation [default=auto]], enable_gtk_doc="$enableval", enable_gtk_doc=auto)
-
-	if test x$enable_gtk_doc = xauto ; then
-	  if test x$GTKDOC = xtrue ; then
-	    enable_gtk_doc=yes
-	  else
-	    enable_gtk_doc=no
-	  fi
-	fi
-
-	dnl NOTE: We need to use a separate automake conditional for this
-	dnl       to make this work with the tarballs.
-	AM_CONDITIONAL(ENABLE_GTK_DOC, test x$enable_gtk_doc = xyes)
-])
-
 AC_DEFUN([GNOME_DEBUG_CHECK],
 [
 	AC_ARG_ENABLE(debug, [  --enable-debug turn on debugging [default=no]], enable_debug="$enableval", enable_debug=no)
