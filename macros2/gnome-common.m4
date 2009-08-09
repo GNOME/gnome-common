GNOME/gnome-common

commit aac1a8ec119d49276257c198327315c234ed2a0d

Chech for single header includes.
When maintainer mode is enabled, check for single header includes as
well as deprecated flags (#557070).

macros2/gnome-common.m4
@@ -38,10 +38,12 @@ AC_DEFUN([GNOME_MAINTAINER_MODE_DEFINES],
 [
 	AC_REQUIRE([AM_MAINTAINER_MODE])
 
+	DISABLE_DEPRECATED=""
 	if test $USE_MAINTAINER_MODE = yes; then
-		DISABLE_DEPRECATED="-DG_DISABLE_DEPRECATED -DGDK_DISABLE_DEPRECATED -DGDK_PIXBUF_DISABLE_DEPRECATED -DPANGO_DISABLE_DEPRECATED -DGTK_DISABLE_DEPRECATED -DGCONF_DISABLE_DEPRECATED -DBONOBO_DISABLE_DEPRECATED -DBONOBO_UI_DISABLE_DEPRECATED -DGNOME_VFS_DISABLE_DEPRECATED -DGNOME_DISABLE_DEPRECATED -DLIBGLADE_DISABLE_DEPRECATED -DWNCK_DISABLE_DEPRECATED -DLIBSOUP_DISABLE_DEPRECATED"
-	else
-		DISABLE_DEPRECATED=""
-	fi
+	        DOMAINS="G ATK PANGO GDK GDK_PIXBUF GTK GCONF BONOBO BONOBO_UI GNOME LIBGLADE VTE GNOME_VFS WNCK LIBSOUP"
+	        for DOMAIN in $DOMAINS; do
+	               DISABLE_DEPRECATED="$DISABLE_DEPRECATED -D${DOMAIN}_DISABLE_DEPRECATED -D${DOMAIN}_DISABLE_SINGLE_INCLUDES"
+	        done
+
 	AC_SUBST(DISABLE_DEPRECATED)
 ])

commit 0620cc2e6ee9be9f877d4d2f4b9eed09be885b27

Fix build breakage in modules caused by commit aac1a8ec

macros2/gnome-common.m4
@@ -44,6 +44,7 @@ AC_DEFUN([GNOME_MAINTAINER_MODE_DEFINES],
 	        for DOMAIN in $DOMAINS; do
 	               DISABLE_DEPRECATED="$DISABLE_DEPRECATED -D${DOMAIN}_DISABLE_DEPRECATED -D${DOMAIN}_DISABLE_SINGLE_INCLUDES"
 	        done
+	fi
 
 	AC_SUBST(DISABLE_DEPRECATED)
 ])